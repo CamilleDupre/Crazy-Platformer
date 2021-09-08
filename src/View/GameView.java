67b8fb2041c1997b1ad23aa380c92c1709b22a93
DamienVerani <47180083+DamienVerani@users.noreply.github.com>
2021-09-09 01:00:26 +0200

    import library

--- src/View/GameView.java
+++ src/View/GameView.java
@@ -1,5 +1,14 @@
 package View;
 
-public class GameView {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameView extends Application{
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }

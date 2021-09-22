984270436ef6acaefed75be3500be326ceec2ba3
CamilleDupre <47179915+CamilleDupre@users.noreply.github.com>
2021-09-22 11:50:17 +0200

    level + rules

--- src/Model/Level.java
+++ src/Model/Level.java
@@ -1,5 +1,34 @@
 package Model;
 
+import java.util.ArrayList;
+
+import javafx.geometry.Point2D;
+
 public class Level {
 
+	int nbLevel;
+	int maxSize;
+	
+	ArrayList<Point2D> ennemies = new ArrayList<Point2D>();
+	ArrayList<Point2D> coins = new ArrayList<Point2D>();
+	ArrayList<Point2D> blocs = new ArrayList<Point2D>();
+	ArrayList<Point2D> powers = new ArrayList<Point2D>();
+	
+	public Level(int lvl , int maxSize, ArrayList<Point2D> ennemies , ArrayList<Point2D> coins , ArrayList<Point2D> blocs, ArrayList<Point2D> powers) {
+	
+		this.nbLevel = lvl;
+		this.maxSize = maxSize;
+		this.ennemies = ennemies;
+		this.coins = coins;
+		this.blocs = blocs;
+		this.powers =powers;
+	}
+	
+	public void addObject(ArrayList<Point2D> Array , Point2D coord ) {
+		Array.add(coord);
+	}
+	
+	public void removeObject(ArrayList<Point2D> Array , Point2D coord ) {
+		Array.remove(coord);
+	}
 }

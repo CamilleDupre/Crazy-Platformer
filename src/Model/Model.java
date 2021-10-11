72a891baa951b847467f678076e97b4d27a895dd
CamilleDupre <47179915+CamilleDupre@users.noreply.github.com>
2021-10-11 22:21:11 +0200

    lvl 2 en cours

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -96,18 +96,18 @@ public class Model {
 		case 0 :
 			menuView.getScene().getStylesheets().remove(0);
 			menuView.getScene().getStylesheets().add(getClass().getClassLoader().getResource("white.css").toExternalForm());
-			
+
 			if(menuView.getGameScene()!=null) {
 				menuView.getGameScene().getStylesheets().remove(0);
 				menuView.getGameScene().getStylesheets().add(getClass().getClassLoader().getResource("white.css").toExternalForm());
 			}
-			
+
 			break;
 
 		case 1 :
 			menuView.getScene().getStylesheets().remove(0);
 			menuView.getScene().getStylesheets().add(getClass().getClassLoader().getResource("dark.css").toExternalForm());
-			
+
 			if(menuView.getGameScene()!=null) {
 				menuView.getGameScene().getStylesheets().remove(0);
 				menuView.getGameScene().getStylesheets().add(getClass().getClassLoader().getResource("dark.css").toExternalForm());
@@ -117,7 +117,7 @@ public class Model {
 		case 2 :
 			menuView.getScene().getStylesheets().remove(0);
 			menuView.getScene().getStylesheets().add(getClass().getClassLoader().getResource("neon.css").toExternalForm());
-			
+
 			if(menuView.getGameScene()!=null) {
 				menuView.getGameScene().getStylesheets().remove(0);
 				menuView.getGameScene().getStylesheets().add(getClass().getClassLoader().getResource("neon.css").toExternalForm());
@@ -130,8 +130,8 @@ public class Model {
 		menuView.getPrimaryStage().show();
 
 	}
-	
-	
+
+
 
 
 
@@ -166,7 +166,7 @@ public class Model {
 				//check if player isn't out of the level in the left side
 				if(player.getPosition().getX() - player.getPlayerSpeed() > 30) {
 					player.setPosition( new Point2D(player.getPosition().getX() - player.getPlayerSpeed(), player.getPosition().getY()));
-					
+
 				}
 			}else if(direction == Control.RIGHT) {
 				//check if player isn't out of the level in the right side
@@ -219,21 +219,21 @@ public class Model {
 				if(b.getPosition().getY() >= player.getPosition().getY() + player.getPlayerSize().getY()-10 && (player.getPosition().getX() + player.getPlayerSize().getX() >= b.getPosition().getX() && player.getPosition().getX() <= b.getPosition().getX()+b.getWidth())){
 					if((int) b.getPosition().getY() < highiestBlock ) {
 						highiestBlock = (int) b.getPosition().getY();
-						
+
 					}
 				}
 			}
 		}
-		
+
 
 		if(player.isJumping()) {
-			
+
 			if(player.getPosition().getY() > maxJumpHeight) {			
 				player.setPosition( new Point2D(player.getPosition().getX(), player.getPosition().getY() - JUMP_FORCE));
 			}else {
 				player.setJumping(false);
 			}
-			
+
 		}else if (player.getPosition().getY() + player.getPlayerSize().getY() < highiestBlock){
 			player.setPosition( new Point2D(player.getPosition().getX(), player.getPosition().getY() + GRAVITY_FORCE));
 			player.setInTheAir(true);
@@ -253,7 +253,7 @@ public class Model {
 				this.player.setLife(this.player.getLife() -1);
 				if(this.player.getLife() == 0) {
 					System.out.println("GAME OVER !!!");
-					
+
 				}else {
 					this.player.setInvincibleAfterAttack(true);
 					// TODO -> false 
@@ -261,7 +261,7 @@ public class Model {
 				break;
 			}
 		}
-		
+
 		for(Point2D trap : currentLevel.getTrap()) {
 			//collision player coins
 			if(player.isPlayerTouchingObject(trap, TRAP_WIDTH, TRAP_HEIGHT) & !this.player.isInvincibleAfterAttack()){	
@@ -281,18 +281,18 @@ public class Model {
 
 	public void checkTreasure() {
 		Point2D treasure =  currentLevel.getTreasure() ;
-			//collision player coins
-			if(player.isPlayerTouchingObject(treasure, TREASURE_WIDTH, TREASURE_HEIGHT) && getNbCoinsCollected() == currentLevel.getMaxCoins() ){	
-				
-				System.out.println("Gagné !!");
-				//here add the victory screen ! 
-				menuView.getControl().displayMenu(gameView.getGamePane(), gameView.getWinMenu());
-				//menuView.getControl().getGravity().interrupt();
-			}
-			else if(player.isPlayerTouchingObject(treasure, TREASURE_WIDTH, TREASURE_HEIGHT) && currentLevel.getCoins().size() < currentLevel.getMaxCoins() ){	
-				
-				System.out.println("Get more coins");
-			}
+		//collision player coins
+		if(player.isPlayerTouchingObject(treasure, TREASURE_WIDTH, TREASURE_HEIGHT) && getNbCoinsCollected() == currentLevel.getMaxCoins() ){	
+
+			System.out.println("Gagné !!");
+			//here add the victory screen ! 
+			menuView.getControl().displayMenu(gameView.getGamePane(), gameView.getWinMenu());
+			//menuView.getControl().getGravity().interrupt();
+		}
+		else if(player.isPlayerTouchingObject(treasure, TREASURE_WIDTH, TREASURE_HEIGHT) && currentLevel.getCoins().size() < currentLevel.getMaxCoins() ){	
+
+			System.out.println("Get more coins");
+		}
 	}
 
 	public void checkCoins() {
@@ -318,7 +318,7 @@ public class Model {
 		ArrayList<Point2D> coins;
 		ArrayList<Point2D> powers;
 		ArrayList<Point2D> traps;
-		
+
 		Point2D treasure;
 
 
@@ -331,14 +331,14 @@ public class Model {
 				{ 
 					//floor
 					add(new Block(new Point2D(0,Model.MIN_FLOOR_HEIGHT),size,50,false));
-					
+
 					//blocks
 					add( new Block(new Point2D(200,Model.MIN_FLOOR_HEIGHT-100),100,100,false));
 					add( new Block(new Point2D(850,Model.MIN_FLOOR_HEIGHT-100),100,100,false));
 					add( new Block(new Point2D(1000,Model.MIN_FLOOR_HEIGHT-270),200,50,false));
 					add( new Block(new Point2D(1250,Model.MIN_FLOOR_HEIGHT-430),200,50,false));
 					add( new Block(new Point2D(1600,Model.MIN_FLOOR_HEIGHT-550),200,550,false));
-					
+
 					//climbing blocks
 					add( new Block(new Point2D(1800,Model.MIN_FLOOR_HEIGHT-170),50,20,false));
 					add( new Block(new Point2D(1800,Model.MIN_FLOOR_HEIGHT-320),50,20,false));
@@ -371,13 +371,13 @@ public class Model {
 			};
 
 			treasure = new Point2D(size - TREASURE_WIDTH ,Model.MIN_FLOOR_HEIGHT - TREASURE_HEIGHT);
-			
+
 			lvl = new Level(lvlId,size,blocks,enemies,coins,powers,traps, treasure);
 
 			break;
-			
+
 		case 2 :
-			
+
 			size = 2600;
 
 			blocks = new ArrayList<Block>() {
@@ -390,23 +390,23 @@ public class Model {
 					add(new Block(new Point2D(750,Model.MIN_FLOOR_HEIGHT-350),250,50,false));
 					add(new Block(new Point2D(1200,Model.MIN_FLOOR_HEIGHT-250),50,50,false));
 					add(new Block(new Point2D(1450,Model.MIN_FLOOR_HEIGHT-150),50,50,false));
-					
+
 					//jump block
 					add(new Block(new Point2D(2200,Model.MIN_FLOOR_HEIGHT-150),50,50,false));
 					add(new Block(new Point2D(2400,Model.MIN_FLOOR_HEIGHT-300),50,50,false));
 					add(new Block(new Point2D(2200,Model.MIN_FLOOR_HEIGHT-450),50,50,false));
 					add(new Block(new Point2D(2400,Model.MIN_FLOOR_HEIGHT-600),50,50,false));
 					add(new Block(new Point2D(2200,Model.MIN_FLOOR_HEIGHT-750),50,50,false));
-					
+
 					//top block
 					add(new Block(new Point2D(1950,Model.MIN_FLOOR_HEIGHT-800),150,50,false));
 					add(new Block(new Point2D(1600,Model.MIN_FLOOR_HEIGHT-800),150,50,false));
 					add(new Block(new Point2D(1100,Model.MIN_FLOOR_HEIGHT-800),250,50,false));
-					
+
 					//Special block
 					add(new Block(new Point2D(720,Model.MIN_FLOOR_HEIGHT-750),50,50,true));
 					add(new Block(new Point2D(1170,Model.MIN_FLOOR_HEIGHT-1100),50,300,true));
-					
+
 				}
 			};		
 
@@ -421,18 +421,18 @@ public class Model {
 					//coins chest
 					add(new Point2D(250,Model.MIN_FLOOR_HEIGHT - 150 - Model.COINS_SIZE));
 					add(new Point2D(850,Model.MIN_FLOOR_HEIGHT -200)); 
-					
-					
+
+
 					//jump
 					add(new Point2D(1800 +400,Model.MIN_FLOOR_HEIGHT-150 - Model.COINS_SIZE)); 
 					add(new Point2D(2000 +400,Model.MIN_FLOOR_HEIGHT-600 - Model.COINS_SIZE)); 
 					add(new Point2D(1800 +400,Model.MIN_FLOOR_HEIGHT-450 - Model.COINS_SIZE)); 
-					
+
 					//top
 					add(new Point2D(700+400,Model.MIN_FLOOR_HEIGHT-800 - Model.COINS_SIZE)); 
 					add(new Point2D(700+400,Model.MIN_FLOOR_HEIGHT-800 - 2 * Model.COINS_SIZE)); 
 					add(new Point2D(1550+400,Model.MIN_FLOOR_HEIGHT-800 - Model.COINS_SIZE)); 
-		
+
 				}
 			};
 
@@ -445,20 +445,77 @@ public class Model {
 					add(new Point2D(600,Model.MIN_FLOOR_HEIGHT -TRAP_HEIGHT));
 					add(new Point2D(1050,Model.MIN_FLOOR_HEIGHT -TRAP_HEIGHT));
 					add(new Point2D(1300,Model.MIN_FLOOR_HEIGHT -TRAP_HEIGHT));
-					
-					
+
+
 					//trap enemie
 					add(new Point2D(1900 ,Model.MIN_FLOOR_HEIGHT -TRAP_HEIGHT));
-					
+
 					//trap jump
 					add(new Point2D(1900 + 400,Model.MIN_FLOOR_HEIGHT -TRAP_HEIGHT));
 				}
 			};
 
 			treasure = new Point2D(820 ,Model.MIN_FLOOR_HEIGHT-340 - TREASURE_HEIGHT);
-			
+
 			lvl = new Level(lvlId,size,blocks,enemies,coins,powers,traps, treasure);
-			
+
+			break;
+
+		case 3 :
+
+			size = 2000;
+
+			blocks = new ArrayList<Block>() {
+				{
+					//Floor Block
+					add(new Block(new Point2D(0,Model.MIN_FLOOR_HEIGHT),size,50,false));
+					//blocks
+				
+
+					//jump block
+					add(new Block(new Point2D(200,Model.MIN_FLOOR_HEIGHT-200),100,25,false));
+					
+					add(new Block(new Point2D(400,Model.MIN_FLOOR_HEIGHT-300),100,25,false));
+					
+					add(new Block(new Point2D(675,Model.MIN_FLOOR_HEIGHT-750),10,400,false));
+					
+					add(new Block(new Point2D(650,Model.MIN_FLOOR_HEIGHT-200),100,25,false));
+					
+					add(new Block(new Point2D(900,Model.MIN_FLOOR_HEIGHT-300),100,25,false));
+					
+					//add(new Block(new Point2D(600,Model.MIN_FLOOR_HEIGHT-450),200,50,false));
+					
+				}
+			};		
+
+			enemies = new ArrayList<Point2D>(){
+				{
+					//add(new Point2D(1950,Model.MIN_FLOOR_HEIGHT - ENEMIES_HEIGHT));	
+				}
+			};
+
+			coins = new ArrayList<Point2D>() {
+				{
+		
+				}
+			};
+
+			powers = new ArrayList<Point2D>(){};
+
+			traps = new ArrayList<Point2D>(){
+				{
+					//trap chest
+					add(new Point2D(500,Model.MIN_FLOOR_HEIGHT -TRAP_HEIGHT));
+					add(new Point2D(650,Model.MIN_FLOOR_HEIGHT -TRAP_HEIGHT));
+					add(new Point2D(800,Model.MIN_FLOOR_HEIGHT -TRAP_HEIGHT));
+					add(new Point2D(950,Model.MIN_FLOOR_HEIGHT -TRAP_HEIGHT));
+				}
+			};
+
+			treasure = new Point2D(820 ,Model.MIN_FLOOR_HEIGHT-340 - TREASURE_HEIGHT);
+
+			lvl = new Level(lvlId,size,blocks,enemies,coins,powers,traps, treasure);
+
 			break;
 
 		default : 
